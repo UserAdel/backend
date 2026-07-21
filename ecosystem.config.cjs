81b07eb3aafb38720a8bd9f2e5576eb986a0e78f
adel hany <adel-hany@edu-lens.com>
2026-07-21 18:45:33 +0300

    fix the ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -14,12 +14,12 @@ module.exports = {
         PORT: 5000,
         MONGODB_URI: "mongodb+srv://adile:adilelove1@cluster0.nkexinn.mongodb.net/Hurghada?appName=Cluster0",
         JWT_SECRET: "nasu_backend_secret_key_2026",
-        CLIENT_URL: "http://localhost:5173",
+        CLIENT_URL: "https://hurghadafrenchguide.com",
         WAPILOT_INSTANCE: "instance3322",
         WAPILOT_TOKEN: "zNgzkd9gmIJ1CMbvxNlyBaA0XEAFFpxk6HK35RLv7q",
         ADMIN_PHONE: "01273809805",
-        BASE_URL: "http://187.124.13.247:5000",
-        FRONTEND_URL: "http://187.124.13.247:5173"
+        BASE_URL: "https://hurghadafrenchguide.com",
+        FRONTEND_URL: "https://hurghadafrenchguide.com"
       },
       log_date_format: "YYYY-MM-DD HH:mm:ss Z",
       error_file: "./logs/pm2-error.log",
